a0675a3653ae34d5e576cf51904b42d635f960d6
HunMyeong <102712296+HunMyeong@users.noreply.github.com>
2022-11-21 10:06:01 +0900

    vscode 1 commit

---
+++ hello.py
@@ -0,0 +1,12 @@
+print("hello")
+print('hello')
+print('11')
+
+a=10
+b=20
+print(a+b)
+inputA = int(input("number1 : "))
+inputB = int(input("number2 : "))
+
+# python 3.x f-string
+print(f"{inputA} + {inputB} = {inputA+inputB}")
